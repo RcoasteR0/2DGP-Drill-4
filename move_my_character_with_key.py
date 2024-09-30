5b3bc10c66d035fdef05ecc53c364e7edf37fdea
RcoasteR0 <st_anthony@naver.com>
2024-09-30 13:25:49 +0900

    handle_events dirx, diry 생성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,7 +7,7 @@ character = load_image('SNES - Dragon Quest 3 JPN - Character Classes.png')
 
 
 def handle_events():
-    global running, dir
+    global running, dirX, dirY
 
     events = get_events()
     for event in events:
@@ -15,21 +15,30 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dirX += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dirY -= 1
+            elif event.key == SDLK_UP:
+                dirX += 1
+            elif event.key == SDLK_DOWN:
+                dirY -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dirX -= 1
             elif event.key == SDLK_LEFT:
-                dir += 1
+                dirX += 1
+            if event.key == SDLK_UP:
+                dirY -= 1
+            elif event.key == SDLK_DOWN:
+                dirY += 1
 
 running = True
 x = 800 // 2
 frame = 0
-dir = 0
+dirX = 0
+dirY = 0
 
 while running:
     clear_canvas()
